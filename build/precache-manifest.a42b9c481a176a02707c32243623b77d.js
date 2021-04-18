19c08a08c6e976bf0cb7a0cd1e14d98539763a76
shubham2270 <shubham2270@gmail.com>
2021-04-18 12:12:27 +0530

    added build folder

---
+++ build/precache-manifest.a42b9c481a176a02707c32243623b77d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4ce797c973d9df944121035c9858c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebf008c47a5c218f1ae9",
+    "url": "/static/css/main.7f32073b.chunk.css"
+  },
+  {
+    "revision": "dd6410db81870c9b9c69",
+    "url": "/static/js/2.482b6268.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.482b6268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf008c47a5c218f1ae9",
+    "url": "/static/js/main.cac639d0.chunk.js"
+  },
+  {
+    "revision": "201768aefbfc397dae8d",
+    "url": "/static/js/runtime-main.6c719300.js"
+  }
+]);
